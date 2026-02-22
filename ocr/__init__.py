3c074e21900e5c752c0e6ae811b029ca0ef722f8
Cat-Not-Furry <rodripachecolopez190@gmail.com>
2026-02-22 00:24:29 -0600

    Corregido

--- ocr/__init__.py
+++ ocr/__init__.py
@@ -1,5 +1,4 @@
-# ocr/__init__.py
 from .engine import run_tesseract, ocr_region
-from .postprocess import clean_text, extract_entities
+from .postprocess import clean_text, estructurar_texto_ocr
 
-__all__ = ["run_tesseract", "ocr_region", "clean_text", "extract_entities"]
+__all__ = ["run_tesseract", "ocr_region", "clean_text", "estructurar_texto_ocr"]
